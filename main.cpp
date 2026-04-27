55288d9c0b52742465a424e20d98a74a75da1d81
Yakov Didenko <kogotto@gmail.com>
2026-04-27 20:38:44 +0300

    Add app stub

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
-int main() {
-    return 0;
+#include <SDL3/SDL.h>
+
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+SDL_AppResult SDL_AppInit(void** gameState, int argc, char** argv) {
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* gameState, SDL_Event* event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* gameState) {
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* gameState, SDL_AppResult result) {
+
 }
