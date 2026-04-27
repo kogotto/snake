51729e1f7952fd07bb188e66ca7ce44c834a8da9
Yakov Didenko <kogotto@gmail.com>
2026-04-27 22:33:29 +0300

    Draw red background on window

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 
+namespace {
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+}
+
 SDL_AppResult SDL_AppInit(void** gameState, int argc, char** argv) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer(
+                "snake by kogo",
+                1024, 768,
+                0,
+                &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(
+            renderer,
+            1024, 768,
+            SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
     return SDL_APP_CONTINUE;
 }
 
@@ -15,6 +38,13 @@ SDL_AppResult SDL_AppEvent(void* gameState, SDL_Event* event) {
 }
 
 SDL_AppResult SDL_AppIterate(void* gameState) {
+    const auto ticks = SDL_GetTicksNS();
+    SDL_Log("Elapsed: %ldms", ticks);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
     return SDL_APP_CONTINUE;
 }
 
