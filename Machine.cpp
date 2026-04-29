9bda1d895d49ffd4782d142fe02b574c383100b2
Yakov Didenko <kogotto@gmail.com>
2026-04-29 21:59:47 +0300

    Delegate logic from Machine to submachines

--- Machine.cpp
+++ Machine.cpp
@@ -1,51 +1,42 @@
 #include <Machine.hpp>
 
 
-namespace {
-const auto points = [] () {
-    std::array<SDL_FPoint, 1000> points;
-    for (int i = 0; i < points.size(); ++i) {
-        points[i].x = SDL_randf() * 1024;
-        points[i].y = SDL_randf() * 768;
-    }
-    return points;
-} ();
-}
-
-
 auto Machine::processEvent(const SDL_Event& event) -> Transition {
     switch (event.type) {
     case SDL_EVENT_QUIT:
         return Transition::exit;
-    case SDL_EVENT_KEY_DOWN:
-        if (event.key.scancode == SDL_SCANCODE_ESCAPE) {
+    }
+
+    switch (current) {
+    case State::mainMenu:
+        switch (mainMenuMachine.processEvent(event)) {
+        case MainMenuMachine::Transition::startGame:
+            current = State::game;
+            return Transition::stay;
+        case MainMenuMachine::Transition::stay:
+            return Transition::stay;
+        case MainMenuMachine::Transition::exit:
             return Transition::exit;
         }
-        break;
+    case State::game:
+        switch (gameMachine.processEvent(event)) {
+        case GameMachine::Transition::stay:
+            return Transition::stay;
+        case GameMachine::Transition::exit:
+            current = State::mainMenu;
+            return Transition::stay;
+        }
     }
-    return Transition::stay;
 }
 
 
 void Machine::iterate(SDL_Renderer* renderer, std::chrono::milliseconds tick) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-    SDL_FRect rect{0.5, 0.5, 300, 300};
-    SDL_RenderFillRect(renderer, &rect);
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-    SDL_RenderPoints(renderer, points.data(), points.size());
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderLine(renderer, 0, 0, 1024, 768);
-    SDL_RenderLine(renderer, 0, 768, 1024, 0);
-
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-    SDL_RenderLines(renderer, points.data(), points.size());
-
-    SDL_RenderPresent(renderer);
+    switch (current) {
+    case State::mainMenu:
+        return mainMenuMachine.iterate(renderer, tick);
+    case State::game:
+        return gameMachine.iterate(renderer, tick);
+    }
 }
 
 
--- Machine.hpp
+++ Machine.hpp
@@ -4,18 +4,27 @@
 
 #include <SDL3/SDL.h>
 
+#include <GameMachine.hpp>
+#include <MainMenuMachine.hpp>
 
 class Machine {
 public:
     enum class Transition {
         stay,
-        exit
+        exit,
     };
 
     Transition processEvent(const SDL_Event& event);
     void iterate(SDL_Renderer* renderer, std::chrono::milliseconds tick);
 
 private:
+    MainMenuMachine mainMenuMachine;
+    GameMachine gameMachine;
+
+    enum class State {
+        mainMenu,
+        game,
+    } current{State::mainMenu};
 };
 
 
