e2d7eb2321e1bc1dff02ba1c539701486330af87
Yakov Didenko <kogotto@gmail.com>
2026-05-01 23:15:15 +0300

    Add food to field

--- GameModel.cpp
+++ GameModel.cpp
@@ -2,10 +2,16 @@
 
 
 GameModel::GameModel() {
+    // snake body
     field_.cell({24, 29}) = Cell::bodyRight;
     field_.cell({24, 30}) = Cell::bodyRight;
     field_.cell({24, 31}) = Cell::bodyRight;
     field_.cell({24, 32}) = Cell::bodyRight;
+
+    // food
+    field_.cell({24, 60}) = Cell::food;
+    field_.cell({12, 11}) = Cell::food;
+    field_.cell({40, 25}) = Cell::food;
 }
 
 
